6d63e459d4186ac77b0ac03aa8adfbf5b9dbd107
AJS <aaronschwehm@googlemail.com>
2022-09-12 17:16:54 +0200

    Added colours

--- checkTLS.ps1
+++ checkTLS.ps1
@@ -2,168 +2,172 @@
 $scriptblock = {
 function get-tlsversions {
 
-Write-Output "------------------------------"
-Write-Output "Computername: $env:COMPUTERNAME"
-Write-Output "Domain: $env:USERDNSDOMAIN"
-Write-Output "Time: $(Get-Date -Format "dd.MM.yyyy | HH:mm")"
-Write-Output "------------------------------"
+Write-Host "------------------------------"
+Write-Host "Computername: $env:COMPUTERNAME"
+Write-Host "Domain: $env:USERDNSDOMAIN"
+Write-Host "Time: $(Get-Date -Format "dd.MM.yyyy | HH:mm")"
+Write-Host "------------------------------"
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: TLS 1.2 enabled"
+    Write-Host "Client: TLS 1.2 enabled" -ForegroundColor Green
 } else {
-Write-Output "Client: TLS 1.2 disabled"
+Write-Host "Client: TLS 1.2 disabled" -ForegroundColor Red
 }
 }
 Catch{
-Write-Output "Client: TLS 1.2 undefined"
+Write-Host "Client: TLS 1.2 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: TLS 1.2 enabled"
+    Write-Host "Server: TLS 1.2 enabled" -ForegroundColor Green
 } else {
-Write-Output "Server: TLS 1.2 disabled"
+Write-Host "Server: TLS 1.2 disabled" -ForegroundColor Red
 }
 }
 Catch{
-Write-Output "Server: TLS 1.2 undefined"
+Write-Host "Server: TLS 1.2 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: TLS 1.1 enabled"
+    Write-Host "Client: TLS 1.1 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: TLS 1.1 disabled"
+Write-Host "Client: TLS 1.1 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: TLS 1.1 undefined"
+Write-Host "Client: TLS 1.1 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: TLS 1.1 enabled"
+    Write-Host "Server: TLS 1.1 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: TLS 1.1 disabled"
+Write-Host "Server: TLS 1.1 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: TLS 1.1 undefined"
+Write-Host "Server: TLS 1.1 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: TLS 1.0 enabled"
+    Write-Host "Client: TLS 1.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: TLS 1.0 disabled"
+Write-Host "Client: TLS 1.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: TLS 1.0 undefined"
+Write-Host "Client: TLS 1.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: TLS 1.0 enabled"
+    Write-Host "Server: TLS 1.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: TLS 1.0 disabled"
+Write-Host "Server: TLS 1.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: TLS 1.0 undefined"
+Write-Host "Server: TLS 1.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: SSL 3.0 enabled"
+    Write-Host "Client: SSL 3.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: SSL 3.0 disabled"
+Write-Host "Client: SSL 3.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: SSL 3.0 undefined"
+Write-Host "Client: SSL 3.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: SSL 3.0 enabled"
+    Write-Host "Server: SSL 3.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: SSL 3.0 disabled"
+Write-Host "Server: SSL 3.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: SSL 3.0 undefined"
+Write-Host "Server: SSL 3.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: SSL 2.0 enabled"
+    Write-Host "Client: SSL 2.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: SSL 2.0 disabled"
+Write-Host "Client: SSL 2.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: SSL 2.0 undefined"
+Write-Host "Client: SSL 2.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: SSL 2.0 enabled"
+    Write-Host "Server: SSL 2.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: SSL 2.0 disabled"
+Write-Host "Server: SSL 2.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: SSL 2.0 undefined"
+Write-Host "Server: SSL 2.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: SSL 2.0 enabled"
+    Write-Host "Client: SSL 2.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: SSL 2.0 disabled"
+Write-Host "Client: SSL 2.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: SSL 2.0 undefined"
+Write-Host "Client: SSL 2.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Server: SSL 2.0 enabled"
+    Write-Host "Server: SSL 2.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: SSL 2.0 disabled"
+Write-Host "Server: SSL 2.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: SSL 2.0 undefined"
+Write-Host "Server: SSL 2.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\PCT 1.0\Client" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: PCT 1.0 enabled"
+    Write-Host "Client: PCT 1.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Client: PCT 1.0 disabled"
+Write-Host "Client: PCT 1.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Client: PCT 1.0 undefined"
+Write-Host "Client: PCT 1.0 undefined" -ForegroundColor Yellow
 }
 
 Try{
 If((get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\PCT 1.0\Server" -Name "Enabled" -ErrorAction Stop).Enabled -ne 0) {
-    Write-Output "Client: PCT 1.0 enabled"
+    Write-Host "Client: PCT 1.0 enabled" -ForegroundColor Red
 } else {
-Write-Output "Server: PCT 1.0 disabled"
+Write-Host "Server: PCT 1.0 disabled" -ForegroundColor Green
 }
 }
 Catch{
-Write-Output "Server: PCT 1.0 undefined"
+Write-Host "Server: PCT 1.0 undefined" -ForegroundColor Yellow
 }
 
-Write-Output "------------------------------"
+Write-Host "------------------------------"
 }
 get-tlsversions
 }
-(Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"').DNSHostname | foreach {Invoke-Command -ComputerName $_ -ScriptBlock $scriptblock}
+try{
+(Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"').DNSHostname | foreach {Invoke-Command -ComputerName $_ -ScriptBlock $scriptblock -ErrorAction Stop}
+}Catch{
+Write-Host $_.Exception.Message -ForegroundColor Red
+}
